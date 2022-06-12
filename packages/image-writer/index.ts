0399ce18c2bbb9258ed12f54326297adff8bef11
michaelballos <ballos.michael@gmail.com>
2022-06-11 22:48:23 -0500

    Add express and image writer endpoint

---
+++ packages/image-writer/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+const app = express();
+const port = 3000;
+const nodeHtmlToImage = require('node-html-to-image');
+
+app.get('/qrcode', async (req: Request, res: Response) => {
+  const image  = await nodeHtmlToImage({
+    html: '<html><body><div>Check out what I just did! #cool</div></body></html>'
+  })
+  res.writeHead(200, { 'Content-Type': 'image/png' });
+  res.end(image, 'binary');
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
