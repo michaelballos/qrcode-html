41e2a039c4526a8f9431353bbd6f60c8f9b08715
michaelballos <ballos.michael@gmail.com>
2022-06-12 13:40:24 -0500

    Edit node html to image import

--- packages/image-writer/index.ts
+++ packages/image-writer/index.ts
@@ -1,16 +1,15 @@
 import express, { Request, Response } from 'express';
+import nodeHtmlToImage from 'node-html-to-image';
 const app = express();
 const port = 3000;
-const nodeHtmlToImage = require('node-html-to-image');
 
 export function ImageWriter() {
-app.get('/qrcode', async (req: Request, res: Response) => {
-  const image  = await nodeHtmlToImage({
-    html: '<html><body><div>Check out what I just did! #cool</div></body></html>'
+  app.get('/qrcode', async (req: Request, res: Response) => {
+    const image  = await nodeHtmlToImage({
+      html: '<html><body><div>Check out what I just did! #cool</div></body></html>'
   })
   res.writeHead(200, { 'Content-Type': 'image/png' });
   res.end(image, 'binary');
 });
-
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+  app.listen(port, () => console.log(`App listening on port ${port}!`));
 }
